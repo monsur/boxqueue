030a443414ac4aa79113ec58facfb80859d47dd4
monsur <monsur@8b94643e-3c53-11de-8c0b-2fbac4a05959>
2009-05-15 02:38:21 +0000

    Added support for param tags

--- boxqueue/src/com/monsur/boxqueue/adaptor/saxhandler/GeneralUrlHandler.java
+++ boxqueue/src/com/monsur/boxqueue/adaptor/saxhandler/GeneralUrlHandler.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Stack;
+import java.util.Map.Entry;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
@@ -27,17 +28,11 @@ public class GeneralUrlHandler extends BaseUrlHandler implements BoxeeHandler {
   @Override
   public void startElement(String nsURI, String strippedName, String tagName,
       Attributes attributes) throws SAXException {
-    // TODO(monsur): Add support for params inside of object tags
-    if (tagName.equalsIgnoreCase("object")) {
+    if (tagName.equalsIgnoreCase("object") || tagName.equalsIgnoreCase("embed") || tagName.equalsIgnoreCase("param")) {
       SimpleElement element = new SimpleElement();
-      element.setName("object");
+      element.setName(tagName.toLowerCase());
       element.setAttributes(getAttributes(attributes));
       stack.add(element);
-    } else if (tagName.equalsIgnoreCase("embed")) {
-      SimpleElement element = new SimpleElement();
-      element.setName("embed");
-      element.setAttributes(getAttributes(attributes));
-      stack.push(element);
     } else if (tagName.equalsIgnoreCase("title")) {
       titleMarker = true;
     }
@@ -45,8 +40,20 @@ public class GeneralUrlHandler extends BaseUrlHandler implements BoxeeHandler {
 
   @Override
   public void endElement( String uri, String localName, String tagName ) {
+    if (stack.size() == 0) {
+      return;
+    }
+    SimpleElement tmp = stack.pop();
     if (tagName.equalsIgnoreCase("object") || tagName.equalsIgnoreCase("embed")) {
-      elements.add(stack.pop());
+      elements.add(tmp);
+    } else if (tagName.equalsIgnoreCase("param")) {
+      SimpleElement tmp2 = stack.pop();
+      if (tmp2.getName().equals("object") || tmp2.getName().equals("embed")) {
+        tmp2.getChildren().add(tmp);
+      }
+      stack.push(tmp2);
+    } else {
+      stack.push(tmp);
     }
   }
 
@@ -74,54 +81,75 @@ public class GeneralUrlHandler extends BaseUrlHandler implements BoxeeHandler {
         elements.add(tmp);
       }
     }
+    // TODO(monsur): Process object tags before embed tags?
+    // TODO(monsur): These videos don't autoplay
     items = new ArrayList<UserItem>();
+    Map<String, Boolean> urlHash = new HashMap<String, Boolean>();
+    System.out.println("Number of elements = " + elements.size());
     for (SimpleElement element : elements) {
+      System.out.println(element.toString());
       UserItem item = null;
       if (element.getName().equals("object")) {
         item = getUserItemFromObject(element);
       } else if (element.getName().equals("embed")) {
         item = getUserItemFromEmbed(element);
       }
-      if (item != null) {
-        if (item.getTitle() == null || item.getTitle() == "") {
-          if (items.size() == 0) {
-            item.setTitle(title);
-          } else {
-            item.setTitle(title + " (" + items.size() + ")");
-          }
-        }
-        items.add(item);
+      if (item == null) {
+        continue;
       }
+      if (urlHash.containsKey(item.getMediaContent().getUrl())) {
+        continue;
+      } else {
+        urlHash.put(item.getMediaContent().getUrl(), true);
+      }
+      if (item.getTitle() == null || item.getTitle() == "") {
+        if (items.size() == 0) {
+          item.setTitle(title);
+        } else {
+          item.setTitle(title + " (" + items.size() + ")");
+        }
+      }
+      items.add(item);
     }
   }
 
   private UserItem getUserItemFromEmbed(SimpleElement element) {
-    String type = element.getAttributes().get("type");
-    if (type == null || !type.equals("application/x-shockwave-flash")) {
+    String embedType = element.getAttributes().get("type");
+    String embedSrc = element.getAttributes().get("src");
+    String flashvars = element.getAttributes().get("flashvars");
+    if (embedType == null || !embedType.equals("application/x-shockwave-flash")) {
       return null;
     }
-    String src = element.getAttributes().get("src");
-    if (src == null) {
+    if (embedSrc == null) {
       return null;
+    }
+    if (flashvars != null) {
+      embedSrc += (embedSrc.indexOf("?") > 0 ? "&" : "?") + flashvars;
     }
     UserItem item = new UserItem();
-    item.getMediaContent().setType(type);
-    item.getMediaContent().setUrl(HelperMethods.getFullUrl(src, url));
+    item.getMediaContent().setType(embedType);
+    item.getMediaContent().setUrl(HelperMethods.getFullUrl(embedSrc, url));
     return item;
   }
 
   private UserItem getUserItemFromObject(SimpleElement element) {
-    String type = element.getAttributes().get("type");
-    if (type == null || !type.equals("application/x-shockwave-flash")) {
+    String objectType = element.getAttributes().get("type");
+    String objectData = element.getAttributes().get("data");
+    String paramMovie = element.getParam("movie");
+    String flashvars = element.getParam("flashvars");
+    String mediaContent = objectData != null ? objectData : paramMovie;
+    if (objectType == null || !objectType.equals("application/x-shockwave-flash")) {
       return null;
     }
-    String data = element.getAttributes().get("data");
-    if (data == null) {
+    if (mediaContent == null) {
       return null;
+    }
+    if (flashvars != null) {
+      mediaContent += (mediaContent.indexOf("?") > 0 ? "&" : "?") + flashvars;
     }
     UserItem item = new UserItem();
-    item.getMediaContent().setType(type);
-    item.getMediaContent().setUrl(HelperMethods.getFullUrl(data, url));
+    item.getMediaContent().setType(objectType);
+    item.getMediaContent().setUrl(HelperMethods.getFullUrl(mediaContent, url));
     return item;
   }
 }
--- boxqueue/src/com/monsur/boxqueue/adaptor/saxhandler/SimpleElement.java
+++ boxqueue/src/com/monsur/boxqueue/adaptor/saxhandler/SimpleElement.java
@@ -1,8 +1,10 @@
 package com.monsur.boxqueue.adaptor.saxhandler;
 
 import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class SimpleElement {
 
@@ -13,6 +15,20 @@ public class SimpleElement {
 
   public SimpleElement() {
     attributes = new HashMap<String, String>();
+    children = new ArrayList<SimpleElement>();
+  }
+
+  public String getParam(String name) {
+    for (SimpleElement child : children) {
+      String paramName = child.getAttributes().get("name");
+      if (paramName != null && paramName.equals(name)) {
+        String paramValue = child.getAttributes().get("value");
+        if (paramValue != null) {
+          return paramValue;
+        }
+      }
+    }
+    return null;
   }
 
   public String getName() {
@@ -41,5 +57,29 @@ public class SimpleElement {
 
   public List<SimpleElement> getChildren() {
     return children;
+  }
+
+  private String getString(int tabs, StringBuilder builder) {
+    if (builder == null) {
+      builder = new StringBuilder();
+    }
+    builder.append("\r\n");
+    for (int i = 0; i < tabs; i++) {
+      builder.append("\t");
+    }
+    tabs++;
+    builder.append(name).append(" ");
+    for (Entry<String, String> attribute : attributes.entrySet()) {
+      builder.append(attribute.getKey() + "=\"" + attribute.getValue() + "\" ");
+    }
+    for (SimpleElement child : children) {
+      child.getString(tabs, builder);
+    }
+    return builder.toString();
+  }
+
+  @Override
+  public String toString() {
+    return getString(0, null);
   }
 }
