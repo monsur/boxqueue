54f7d9afaa09be3373387e596dfec073c8b30699
monsur <monsur@8b94643e-3c53-11de-8c0b-2fbac4a05959>
2009-06-15 02:58:46 +0000

    Updated certain string fields to Text fields, Fixed so items are properly removed from the queue.

--- trunk/boxqueue/src/com/monsur/boxqueue/data/MediaThumbnail.java
+++ trunk/boxqueue/src/com/monsur/boxqueue/data/MediaThumbnail.java
@@ -4,22 +4,33 @@ import javax.jdo.annotations.EmbeddedOnly;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 
+import com.google.appengine.api.datastore.Text;
+
 @PersistenceCapable
 @EmbeddedOnly
 public class MediaThumbnail {
 
   @Persistent
-  private String mediaThumbnailUrl = "";
+  private String mediaThumbnailUrl;
+  @Persistent
+  private Text mediaThumbnailUrl2;
 
   public boolean exists() {
-    return mediaThumbnailUrl != null && mediaThumbnailUrl != "";
+    return (mediaThumbnailUrl != null && mediaThumbnailUrl != "") || (mediaThumbnailUrl2 != null &&
+        mediaThumbnailUrl2.getValue() != "");
   }
 
   public String getUrl() {
-    return mediaThumbnailUrl;
+    if (mediaThumbnailUrl2 != null) {
+      return mediaThumbnailUrl2.getValue();
+    } else if (mediaThumbnailUrl != null) {
+      return mediaThumbnailUrl;
+    }
+    return "";
   }
 
   public void setUrl(String url) {
-    this.mediaThumbnailUrl = url;
+    this.mediaThumbnailUrl2 = new Text(url);
+    this.mediaThumbnailUrl = null;
   }
 }
